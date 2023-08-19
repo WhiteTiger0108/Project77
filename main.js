16337c5f48cfae83bc108f6c63fb3e18f4c06c16
WhiteTiger0108 <111655750+WhiteTiger0108@users.noreply.github.com>
2023-08-19 10:03:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Manasvi ";
+var last_name = "Nalasani";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 4;
+var b = 2;
+var add = a + b;
+var divide = a / b;
+var multiply = a * b;
+var subtract = a - b;
+console.log(add);
+console.log(divide);
+console.log(multiply);
+console.log(subtract);
